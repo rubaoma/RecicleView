6d1276d44651e1865a5fb21a136dd34348774493
Rubens <rubao.ma@gmail.com>
2020-09-19 14:21:44 -0300

    first commit

---
+++ app/src/main/java/com/example/recycleview/activity/activity/activity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.recycleview.activity.activity.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.recycleview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycleView);
+
+        // configurar o adapter
+
+
+
+        //configurar RecycleView
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+//        recyclerView.setAdapter();
+
+    }
+}
